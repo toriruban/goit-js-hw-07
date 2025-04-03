e6026a2501be56101b3bb3c6b76226529349e8d2
ToriRuban <toriruban@icloud.com>
2025-04-03 13:55:44 +0300

    Complete task 5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const buttonElement = document.querySelector('.change-color');
+const colorSpan = document.querySelector('.color');
+
+buttonElement.addEventListener('click', () => {
+  const newColor = getRandomHexColor();
+  document.body.style.backgroundColor = newColor;
+  colorSpan.textContent = newColor;
+});
